a921bd55ee3cba828a39baac6420a6c6272eb567
femi <latitude81uk@yahoo.com>
2018-08-23 14:00:01 -0400

    added database

--- main.go
+++ main.go
@@ -1,11 +1,47 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 
-	"github.com/golang/examle/stringutil"
+	_ "github.com/denisenkom/go-mssqldb"
 )
 
+// func Routes() *chi.Mux {
+// 	router.Get("/posts", AllPosts)
+//     router.Get("/posts/{id}", DetailPost)
+//     return router
+// }func Routes() *chi.Mux {
+// 	router.Get("/posts", AllPosts)
+//     router.Get("/posts/{id}", DetailPost)
+//     return router
+// }
+
 func main() {
-	fmt.Println(stringutil.Reverse("!testing"))
+
+}
+func init() {
+
+	condb, errdb := sql.Open("mssql", "server=localhost\\Standard;user id=sa;password=prestige;database=Epcrysystem")
+
+	var pcrID string
+
+	if errdb != nil {
+		fmt.Println("Error opening db:", errdb.Error())
+	}
+
+	rows, err := condb.Query("select * from Organization")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		err := rows.Scan(&pcrID)
+		if err != nil {
+
+		}
+	}
+
 }
